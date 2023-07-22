af5e6b23cc98bb5e91d4dc206ff2fb8b9a2917b6
Djennaoui Raouf <djennaoui.raouf@gmail.com>
2023-07-22 16:52:33 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import NavBar from "./components/NavBar";
 import AboutMe from "./components/AboutMe";
 import Footer from "./components/Footer";
 import Skills from "./components/Skills";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<AboutMe />} />
+                <Route path="/about" element={<AboutMe />} />
+                <Route path="/dev" element={<Skills label={"dev"} data={[{img:"",skill_label:"c++",level:3}]} />} />
 
-        <AboutMe/>
-        <Footer/>
+            </Routes>
+        </BrowserRouter>
+
+      <Footer/>
     </div>
   );
 }
